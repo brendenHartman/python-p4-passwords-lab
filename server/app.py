ec9116d2aa17169944e83e4c726c9476254ecdb9
Brenden Hartman <brendenhart22@gmail.com>
2024-06-07 15:40:19 -0500

    workedd wiith another authorization in flask

--- server/app.py
+++ server/app.py
@@ -25,17 +25,31 @@ class Signup(Resource):
         user.password_hash = json['password']
         db.session.add(user)
         db.session.commit()
+        session['user_id'] = user.id
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
-
+    def get(self):
+        if not session['user_id']:
+            return '', 204
+        else:
+            return User.query.filter_by(id=session['user_id']).first().to_dict() ,200
+        
 class Login(Resource):
-    pass
-
+    def post(self):
+        username=request.get_json().get('username')
+        user = User.query.filter_by(username=username).first()
+        session['user_id'] = user.id
+        return user.to_dict()
+    
 class Logout(Resource):
-    pass
+    def delete(self): 
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
 
+api.add_resource(CheckSession,'/check_session')
+api.add_resource(Login,'/login')
+api.add_resource(Logout,'/logout')
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
 
